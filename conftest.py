6dea29e62dbb4078e3b231064ba2c094438ff292
Aleksandr Zhukov <hyperpugs@yandex.ru>
2024-12-08 16:53:28 +0300

    first

---
+++ conftest.py
@@ -0,0 +1,42 @@
+import pytest
+import random
+import string
+import json
+
+@pytest.fixture
+def payload():
+    def generate_random_string(length):
+        letters = string.ascii_lowercase
+        random_string = ''.join(random.choice(letters) for i in range(length))
+        return random_string
+
+    # генерируем логин, пароль и имя курьера
+    login = generate_random_string(10)
+    password = generate_random_string(10)
+    first_name = generate_random_string(10)
+
+    # собираем тело запроса
+    payload = {
+        "login": login,
+        "password": password,
+        "firstName": first_name
+    }
+
+    return payload
+
+@pytest.fixture
+def order_data():
+    order_data = {
+        "firstName": "Naruto",
+        "lastName": "Uchiha",
+        "address": "Konoha, 142 apt.",
+        "metroStation": 4,
+        "phone": "+7 800 355 35 35",
+        "rentTime": 5,
+        "deliveryDate": "2025-06-06",
+        "comment": "Saske, come back to Konoha"
+    }
+    order_data = json.dumps(order_data)
+    return order_data
+
+
