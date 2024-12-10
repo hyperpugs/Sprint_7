fba5114a112354befea1e5be289e085cc4e79291
Sasha <78263390+hyperpugs@users.noreply.github.com>
2024-12-10 12:38:11 +0300

    Update test_courier_login.py

--- tests/test_courier_login.py
+++ tests/test_courier_login.py
@@ -1,39 +1,32 @@
 import requests
-import json
-import pytest
 import allure
-from urls import CourierLoginUrls
+import generators
 
-class TestCourierLogin:
+from data import Url
+
+
+class TestLoginCourier:
+
     @allure.title("Проверяем успешный логин курьера")
-    def test_login_is_successful(self, payload):
-        response = requests.post(CourierLoginUrls.create, data=payload)
-        assert response.status_code == 201 and response.text == '{"ok":true}'
-        login = payload.get("login")
-        password = payload.get("password")
-        response = requests.post(CourierLoginUrls.login, data={"login": login, "password": password})
-        assert response.status_code == 200 and "id" in response.json()
+    def test_successful_courier_login(self, create_courier):
+        response = requests.post(f'{Url.MAIN_URL}{Url.COURIER_LOGIN}', json=create_courier[1])
+        courier_id = response.json()
+        assert response.status_code == 200 and "id" in courier_id
+
+    @allure.title("Проверяем, что без предварительного создания пользователя приходит ошибка")
+    def test_unregistered_courier_login(self):
+        login_data = {'login': generators.login_generator(), 'password': generators.password_generator()}
+        response = requests.post(f'{Url.MAIN_URL}{Url.COURIER_LOGIN}', login_data)
+        assert response.status_code == 404 and (response.json()["message"] == 'Учетная запись не найдена')
 
     @allure.title("Проверяем, что при передаче только логина приходит ошибка")
-    def test_login_without_password_returns_error(self, payload):
-        response = requests.post(CourierLoginUrls.create, data=payload)
-        assert response.status_code == 201 and response.text == '{"ok":true}'
-        login = payload.get("login")
-        response = requests.post(CourierLoginUrls.login, data={"login": login})
-        assert response.status_code == 400 and response.json()["message"] == "Недостаточно данных для входа"
+    def test_courier_login_empty_password_error(self, create_courier):
+        data_response = {'login': create_courier[2], 'password': ''}
+        response = requests.post(f'{Url.MAIN_URL}{Url.COURIER_LOGIN}', json=data_response)
+        assert response.status_code == 400 and (response.json()["message"] == "Недостаточно данных для входа")
 
     @allure.title("Проверяем, что при передаче только пароля приходит ошибка")
-    def test_login_without_login_returns_error(self, payload):
-        response = requests.post(CourierLoginUrls.create, data=payload)
-        assert response.status_code == 201 and response.text == '{"ok":true}'
-        password = payload.get("password")
-        response = requests.post(CourierLoginUrls.login, data={"password": password})
-        assert response.status_code == 400 and response.json()["message"] == "Недостаточно данных для входа"
-
-    @allure.title("Проверяем, что без предварительного создания пользователя приходит ошибка, т.к. такого пользователя нет")
-    def test_login_nonexistent_user_returns_error(self, payload):
-        login = payload.get("login")
-        password = payload.get("password")
-        response = requests.post(CourierLoginUrls.login, data={"login": login, "password": password})
-        assert response.status_code == 404 and response.json()["message"] == "Учетная запись не найдена"
-
+    def test_courier_login_empty_login_error(self, create_courier):
+        data_response = {'login': '', 'password': create_courier[3]}
+        response = requests.post(f'{Url.MAIN_URL}{Url.COURIER_LOGIN}', json=data_response)
+        assert response.status_code == 400 and (response.json()["message"] == "Недостаточно данных для входа")
