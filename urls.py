6d92694fb513573ba6310b1c772802e605e43120
Sasha <78263390+hyperpugs@users.noreply.github.com>
2024-12-10 12:39:48 +0300

    Update and rename urls.py to data.py

--- urls.py
+++
@@ -1,6 +0,0 @@
-class CourierLoginUrls:
-    create = "https://qa-scooter.praktikum-services.ru/api/v1/courier"
-    login = "https://qa-scooter.praktikum-services.ru/api/v1/courier/login"
-
-class OrderUrls:
-    order = "https://qa-scooter.praktikum-services.ru/api/v1/orders"
