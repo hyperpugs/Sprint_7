72f28fa0d30379b6c22f45bf9149901fd8aaeb11
Sasha <78263390+hyperpugs@users.noreply.github.com>
2024-12-10 12:38:35 +0300

    Update test_create_order.py

--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -2,15 +2,16 @@ import requests
 import json
 import pytest
 import allure
-from urls import OrderUrls
+from data import Url, DataForOrder
 
 
 class TestCreateOrder:
-    colors = ["", '"color": ["BLACK"]', '"color": ["GREY"]', '"color": ["BLACK", "GREY"]']
-    @allure.title("Проверяем успешное создание заказа со всеми обязательными полями и разными комбинациями цветов")
-    @pytest.mark.parametrize("color", colors)
-    def test_order_is_created(self, order_data, color):
-        order_data += color
-        order_data = json.dumps(order_data)
-        response = requests.post(OrderUrls.order, data=order_data)
-        assert response.status_code == 201 and "track" in response.json()
+
+    @allure.title("Проверяем успешное создание заказа")
+    @pytest.mark.parametrize('scooter_color', DataForOrder.scooter_color)
+    def test_create_order_with_diff_colors(self, scooter_color):
+        order_data = DataForOrder.order_data
+        order_data['color'] = scooter_color
+        order = requests.post(f'{Url.MAIN_URL}{Url.CREATE_ORDER}', json=order_data)
+        assert order.status_code == 201 and 'track' in order.json()
+        requests.put(f'{Url.MAIN_URL}{Url.ORDER_CANCEL}{order.json()['track']}')
