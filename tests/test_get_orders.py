be61db8fccefc7240865bf05d6452df36f1a4ed5
Sasha <78263390+hyperpugs@users.noreply.github.com>
2024-12-10 12:38:50 +0300

    Update test_get_orders.py

--- tests/test_get_orders.py
+++ tests/test_get_orders.py
@@ -1,19 +1,11 @@
 import requests
-import json
-import pytest
 import allure
-from urls import CourierLoginUrls, OrderUrls
+from data import Url
 
-class TestGetOrders:
-    @allure.title("Проверяем успешное получение списка заказов по id курьера")
-    def test_get_order_list_returns_body(self, payload):
-        response = requests.post(CourierLoginUrls.create, data=payload)
-        assert response.status_code == 201 and response.text == '{"ok":true}'
-        login = payload.get("login")
-        password = payload.get("password")
-        response = requests.post(CourierLoginUrls.login, data={"login": login, "password": password})
-        created_courier = response.json()
-        courier_id = created_courier['id']
-        params = {"courierId" : courier_id}
-        response = requests.get(OrderUrls.order, params=params)
-        assert response.status_code == 200 and "orders" in response.text
+
+class TestOrdersList:
+
+    @allure.title("Проверяем успешное получение списка заказов")
+    def test_successful_get_order_list(self):
+        response = requests.get(f'{Url.MAIN_URL}{Url.GET_ORDER_LIST}')
+        assert response.status_code == 200 and 'orders' in response.json()
