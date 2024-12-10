ed48fbfd673df9324e9d4ccab623aa6cf420398d
Sasha <78263390+hyperpugs@users.noreply.github.com>
2024-12-10 12:39:02 +0300

    Update tets_create_courier.py

--- tests/tets_create_courier.py
+++ tests/tets_create_courier.py
@@ -1,42 +1,23 @@
 import requests
-import json
 import pytest
 import allure
-from urls import CourierLoginUrls
+from data import  Url, DataForRegistration
 
-class TestCreateCourier:
+
+class TestsCreateNewCourier:
 
     @allure.title("Проверяем, что курьер успешно создан")
-    def test_courier_is_successfully_created(self, payload):
-        response = requests.post(CourierLoginUrls.create, data = payload)
-        assert response.status_code == 201 and response.text == '{"ok":true}'
+    def test_creation_courier_success(self, generate_courier_data):
+        registration = requests.post(f'{Url.MAIN_URL}{Url.CREATE_COURIER}', json=generate_courier_data[0])
+        assert registration.status_code == 201 and (registration.json() == '{"ok":true}')
 
     @allure.title("Проверяем, что нельзя создать дубликат курьера")
-    def test_cannot_create_duplicate_courier(self, payload):
-        first_courier = payload
-        response = requests.post(CourierLoginUrls.create, data=first_courier)
-        assert response.status_code == 201 and response.text == '{"ok":true}'
-        second_courier = first_courier
-        response = requests.post(CourierLoginUrls.create, data=second_courier)
-        assert response.status_code == 409 and response.json()["message"] == "Этот логин уже используется. Попробуйте другой."
+    def test_creation_courier_clone_error(self, create_courier):
+        response = requests.post(f'{Url.MAIN_URL}{Url.CREATE_COURIER}', json=create_courier[0])
+        assert response.status_code == 409 and (response.json() == 'Этот логин уже используется. Попробуйте другой.')
 
-    @allure.title("Проверяем, что при передаче только имени (без логина и пароля) нельзя создать курьера")
-    def test_only_firstname_returns_error(self, payload):
-        firstname = payload.get("firstName")
-        response = requests.post(CourierLoginUrls.create, data={"firstName": firstname})
-        assert response.status_code == 400 and response.json()["message"] == "Недостаточно данных для создания учетной записи"
-
-    @allure.title("Проверяем, что при передаче только логина и имени (без  пароля) нельзя создать курьера")
-    def test_without_password_returns_error(self, payload):
-        login = payload.get("login")
-        firstname = payload.get("firstName")
-        response = requests.post(CourierLoginUrls.create,data={"login": login, "firstName":firstname})
-        assert response.status_code == 400 and response.json()["message"] == "Недостаточно данных для создания учетной записи"
-
-    @allure.title("Проверяем, что при передаче только имени и пароля(без логина) нельзя создать курьера")
-    def test_without_password_returns_error(self, payload):
-        password = payload.get("password")
-        firstname = payload.get("firstName")
-        response = requests.post(CourierLoginUrls.create, data={"password": password, "firstName": firstname})
-        assert response.status_code == 400 and response.json()["message"] == "Недостаточно данных для создания учетной записи"
-
+    @allure.title( "Проверяем, что без передачи логина или пароля  нельзя создать курьера")
+    @pytest.mark.parametrize('data_setup', DataForRegistration.reg_data)
+    def test_creation_courier_deficit_data_error(self, data_setup):
+        response = requests.post(f'{Url.MAIN_URL}{Url.CREATE_COURIER}', data_setup)
+        assert response.status_code == 400 and (response.json() == 'Недостаточно данных для создания учетной записи')
